96285d322727eaace63cfaff5e5e8f40a571eedf
MaroofAkram <44575901+MaroofAkram@users.noreply.github.com>
2018-12-26 17:59:15 +0500

    trees activity done

---
+++ 2.php
@@ -0,0 +1,10 @@
+<?php
+$a=array("Trees can reduce air temperature by blocking sunlight. Further cooling occurs when water evaporates from the leaf surface. The conversion of water to air vapor --- a chemical process --- removes heat energy from the air.","A tree can be a natural air conditioner. The evaporation from a single tree can produce the cooling effect of 10 room size air conditioners operating 20 hours a day.","You can improve the efficiency of your heat pump by shading it with a tree.");
+for($y=0;$y<3;$y++)
+{
+ 
+    echo $a[$y]; 
+   
+ 
+}
+?>
